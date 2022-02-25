ffc601eefac09aec137fa132b2291c4fea3597c6
Thanos Sgouros <athanasios.sgouros@students.plymouth.ac.uk>
2022-02-25 02:31:16 +0000

    Commit 1.3. Flashlight Improvements
    Animations added.
    Flashlight Controller.cs added.
    
    Now the player can press the button "G" to turn on/off the flashlight.
    Coroutines added as well for a short delay on the "spot light" to make it look a bit better and independent as well.
    
    The hand now also has a simple skin color.

---
+++ Comp3000/Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    public Animator anim;
+
+    float lastTime;
+
+    private GameObject lightObject;
+    private Light myLightComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTime = Time.time;
+        lightObject = GameObject.Find("Spot Light");
+        myLightComponent = lightObject.GetComponent<Light>();
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("g") && (Time.time - lastTime > 1))
+        {
+            if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("Show Flashlight"))
+            {
+                myLightComponent.enabled = !myLightComponent;
+
+                StartCoroutine(ExecuteAfterTime(.25f));
+                IEnumerator ExecuteAfterTime(float time)
+                {
+                    yield return new WaitForSeconds(time);
+
+                    anim.Play("Hide Flashlight");
+                }   
+            }
+            else if(this.anim.GetCurrentAnimatorStateInfo(0).IsName("Hide Flashlight"))
+            {
+                anim.Play("Show Flashlight");
+
+                StartCoroutine(ExecuteAfterTime(1));
+                IEnumerator ExecuteAfterTime(float time)
+                {
+                    yield return new WaitForSeconds(time);
+
+                    myLightComponent.enabled = myLightComponent;
+                }
+            }
+            lastTime = Time.time;
+        }
+    }
+}
