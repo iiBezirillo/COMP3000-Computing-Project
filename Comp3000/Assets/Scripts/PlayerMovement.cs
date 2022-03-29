7e74575b9bfca2d501438150e84f15946db2ba29
Thanos Sgouros <athanasios.sgouros@students.plymouth.ac.uk>
2022-03-29 05:28:45 +0100

    Commit 1.4 Movement improvement
    Jump is Disabled. (As a horror game, jump is not required)
    Added Crouch mechanic, Player can now crouch by pressing "C" and has exit times.
    Added Sprint, Player can Sprint using "Left Shift". Sprint and crouching do not intervene with each other, meaning player can crouch and when on crouch sprint is not available, and player can sprint but cannot crouch at the same time. This gives a smoother experience on player movement.

--- Comp3000/Assets/Scripts/PlayerMovement.cs
+++ Comp3000/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12;
+    public float speed = 10;
+
     public float gravity = -9.81f;
     //jump
-    public float jumpHeight = 3f;
+    //public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -18,6 +19,16 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    float lastTime;
+
+    public Animator crouchAnim;
+
+    private void Start()
+    {
+        lastTime = Time.time;
+
+        crouchAnim = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -36,13 +47,54 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
 
         //Jump
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-        }
+        //if (Input.GetButtonDown("Jump") && isGrounded)
+        //{
+        //    velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        //}
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown("c") && (Time.time - lastTime > 1))
+        {
+            if(speed == 10f)
+            {
+                this.crouchAnim.GetCurrentAnimatorStateInfo(0).IsName("Crouch OFF");
+                speed = speed - 5f;
+                crouchAnim.Play("Crouch ON");
+            }
+            else if(speed == 5f)
+            {
+                this.crouchAnim.GetCurrentAnimatorStateInfo(0).IsName("Crouch ON");
+
+                StartCoroutine(ExecuteAfterTime(.30f));
+                IEnumerator ExecuteAfterTime(float time)
+                {
+                    yield return new WaitForSeconds(time);
+                    
+                    speed = speed + 5f;
+
+                }
+                crouchAnim.Play("Crouch OFF");
+            }
+            lastTime = Time.time;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (speed == 10)
+            {
+                speed = speed + 5;
+
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (speed == 15f)
+            {
+                speed = speed - 5f;
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -11,3 +11,11 @@ arm flashlight: https://blendswap.com/blend/19747
 Skybox: https://assetstore.unity.com/packages/2d/textures-materials/sky/skybox-series-free-103633
 
 flashlight sway: https://www.youtube.com/watch?v=hifCUD3dATs
+
+flashlight Hide/Show: https://answers.unity.com/questions/362629/how-can-i-check-if-an-animation-is-being-played-or.html
+
+button disable for a few sec: https://answers.unity.com/questions/774544/how-do-you-disable-a-key-for-5-seconds.html
+
+Spot Light object disable/enable: https://answers.unity.com/questions/1401879/toggle-light-component-on-and-off.html
+
+Coroutine for flashlight to turn on after few sec: https://answers.unity.com/questions/796881/c-how-can-i-let-something-happen-after-a-small-del.html
