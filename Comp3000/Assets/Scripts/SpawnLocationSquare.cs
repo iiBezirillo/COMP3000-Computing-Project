2dbec89aec0e9c0d07976d3d01ba60b1594bcd59
Thanos Sgouros <athanasios.sgouros@students.plymouth.ac.uk>
2022-04-26 01:54:40 +0300

    Commit 3.9 Assets added
    Added A car, and a church asset as prefabs and to the terrain.
    Added black liquid holes to the terrain. The big one are deadly and can kill you.
    Added Colliders to the world where if the player step in, Scp-106 will spawn.

--- Comp3000/Assets/Scripts/SpawnLocationSquare.cs
+++ Comp3000/Assets/Scripts/SpawnLocationSquare.cs
@@ -15,15 +15,15 @@ public class SpawnLocationSquare : MonoBehaviour
     {
 
         //when space is pressed
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //enable SCP-106
-            SCP106.SetActive(true);
-            //make sure he spawns correctly to the ground
-            SCP106.transform.position = SpawnLocation.transform.position;
-            SCP106.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-            SCP106.transform.SetParent(null);
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    //enable SCP-106
+        //    SCP106.SetActive(true);
+        //    //make sure he spawns correctly to the ground
+        //    SCP106.transform.position = SpawnLocation.transform.position;
+        //    SCP106.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        //    SCP106.transform.SetParent(null);
+        //}
 
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         //transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
@@ -34,5 +34,15 @@ public class SpawnLocationSquare : MonoBehaviour
         //a "window" that helps me see the spawn location
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(this.transform.position, radius);
+    }
+
+    public void spawnTrigger()
+    {
+        //enable SCP-106
+        SCP106.SetActive(true);
+        //make sure he spawns correctly to the ground
+        SCP106.transform.position = SpawnLocation.transform.position;
+        SCP106.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        SCP106.transform.SetParent(null);
     }
 }
---
+++ Comp3000/Assets/Scripts/scpSpawnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scpSpawnTrigger : MonoBehaviour
+{
+    public GameObject spawnTrigger;
+    public GameObject spawnLocation;
+    SpawnLocationSquare spawnLocationSquare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocationSquare = spawnLocation.GetComponent<SpawnLocationSquare>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        spawnLocationSquare.spawnTrigger();
+        spawnTrigger.SetActive(false);
+        
+    }
+}
